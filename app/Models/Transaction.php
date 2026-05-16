cc9de91f19f0d5977ed14476a516caf6d6d8f2ee
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-16 23:52:45 +0100

    Create Transaction model, and Add Relationship with users table and add docs for the class

---
+++ app/Models/Transaction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TransactionType;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property TransactionType $type
+ * @property-read \App\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction query()
+ * @mixin \Eloquent
+ */
+class Transaction extends Model
+{
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'amount' => 'decimal:2',
+            'type' => TransactionType::class,
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
