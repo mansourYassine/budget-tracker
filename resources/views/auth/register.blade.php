55ed376a84b7e5b090bd753ec4a9334997b9a2cb
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-19 21:10:43 +0100

    Add validation error messages to the registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,15 +18,24 @@
                     <form action="/register" method="post" class="mt-5">
                         <div class=" flex flex-col mb-4">
                             <label for="form-name" class=" text-sm font-medium text-gray-600 mb-2.5 ">FULL NAME</label>
-                            <input type="text" name="name" id="form-name" placeholder="John Doe" class=" border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3">
+                            <input type="text" name="name" id="form-name" placeholder="John Doe" value="{{ old('name') }}" class=" @error('name') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3">
+                            @error('name')
+                                <div class="text-red-500 text-sm">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class=" flex flex-col mb-4">
                             <label for="form-email"  class=" text-sm font-medium text-gray-600 mb-2.5 ">EMAIL ADDRESS</label>
-                            <input type="email" name="email" id="form-email" placeholder="name@email.com" class=" border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
+                            <input type="email" name="email" id="form-email" placeholder="name@email.com"  value="{{ old('email') }}" class=" @error('email') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
+                            @error('email')
+                                <div class="text-red-500 text-sm">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class=" flex flex-col mb-4">
                             <label for="form-pass"  class=" text-sm font-medium text-gray-600 mb-2.5 ">PASSWORD</label>
-                            <input type="password" name="password" id="form-pass" placeholder="••••••••" class=" border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
+                            <input type="password" name="password" id="form-pass" placeholder="••••••••" class=" @error('password') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
+                            @error('password')
+                                <div class="text-red-500 text-sm">{{ $message }}</div>
+                            @enderror
                         </div>
                         <button type="submit" class=" bg-secondary text-white font-medium px-5 py-3 rounded-md w-full mt-3 cursor-pointer">Create Account</button>
                     </form>
