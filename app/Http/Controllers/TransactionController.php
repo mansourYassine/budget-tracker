60dd664abdd081a77b4d00c35a37397a8a7a274a
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-19 23:32:36 +0100

    create the transaction controller class

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the transaction.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new transaction.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created transaction in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified transaction.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified transaction.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified transaction in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified transaction from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
