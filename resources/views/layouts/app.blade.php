9bbbff83d0456b9d67ac7f9bec3819101b6ded1f
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-20 20:28:27 +0100

    make the layout title dynamic, and add a margin bottom to the sidebar toggle button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,12 @@
                 <li class="navlink"><a class=" {{ request()->routeIs('dashboard') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href="{{ route('dashboard') }}"><i class="fa-solid fa-house"></i> <span class=" ">Dashboard</span></a></li>
                 <li class="navlink"><a class=" {{ request()->routeIs('transactions.*') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href="{{ route('transactions.index') }}"><i class="fa-solid fa-receipt"></i> <span class=" ">Transactions</span></a></li>
                 <li class="navlink"><a class=" {{ request()->routeIs('profile') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href=""><i class="fa-solid fa-user"></i> <span class=" ">Profile</span></a></li>
-                <li class=" sidebar-toggle mt-auto"><a class=" text-sm font-bold flex items-center gap-2 mb-1 py-2.5 px-2 rounded-sm bg-white text-blue-600" href="#"><i class="fa-solid fa-angles-right fa-xl rotate-180"></i> <span class=" text-[17px]">Collapse</span></a></li>
+                <li class=" sidebar-toggle mt-auto mb-4"><a class=" text-sm font-bold flex items-center gap-2 mb-1 py-2.5 px-2 rounded-sm bg-white text-blue-600" href="#"><i class="fa-solid fa-angles-right fa-xl rotate-180"></i> <span class=" text-[17px]">Collapse</span></a></li>
             </ul>
         </aside>
         <div class=" right-page h-screen flex flex-col ml-16 md:ml-60">
             <header class=" py-4.5 bg-white shadow-xs z-10 ">
-                <h2 class=" text-secondary text-2xl font-bold text-center ">Budget Tracker</h2>
+                <h2 class=" text-secondary text-2xl font-bold ml-5 ">@yield('title')</h2>
             </header>
             <main class=" flex flex-1 justify-center bg-backgr ">
             </main>
