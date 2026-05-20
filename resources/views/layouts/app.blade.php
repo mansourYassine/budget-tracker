8b5491ce18cd455d9e22cef774187a81bda29509
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-20 20:19:46 +0100

    Make the sidebar responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,25 +6,26 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>@yield('title')</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @stack('scripts')
     </head>
     <body>
-        <div class="flex">
-            <aside class=" w-60 bg-blue-950 text-white ">
-                <h2 class=" text-2xl font-semibold my-5 ml-7 ">Budget Tracker</h2>
-                <ul class=" mx-7 mt-10">
-                    <li><a class=" {{ request()->routeIs('dashboard') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold block mb-1 py-2.5 pl-2 rounded-sm" href="{{ route('dashboard') }}"><i class="fa-solid fa-house"></i> <span>Dashboard</span></a></li>
-                    <li><a class=" {{ request()->routeIs('transactions.*') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold block mb-1 py-2.5 pl-2 rounded-sm" href="{{ route('transactions.index') }}"><i class="fa-solid fa-receipt"></i> <span>Transactions</span></a></li>
-                    <li><a class=" {{ request()->routeIs('profile') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold block mb-1 py-2.5 pl-2 rounded-sm" href=""><i class="fa-solid fa-user"></i> <span>Profile</span></a></li>
-                </ul>
-            </aside>
-            <div class=" flex-1 h-screen flex flex-col">
-                <header class=" py-4.5 bg-white shadow-xs z-10 ">
-                    <h2 class=" text-secondary text-2xl font-bold text-center ">Budget Tracker</h2>
-                </header>
-                <main class=" flex flex-1 justify-center bg-backgr ">
-                </main>
+        <aside class=" fixed top-0 left-0 bottom-0 z-20 w-60 bg-blue-950 text-white flex flex-col ">
+            <div class="my-5 mx-6">
+                <h2 class=" text-logo text-2xl font-semibold ">Budget Tracker</h2>
+                <span class=" logo text-1xl font-bold h-8 hidden ">BT</span>
             </div>
+            <ul class=" mx-4 mt-2 h-full flex flex-col">
+                <li class="navlink"><a class=" {{ request()->routeIs('dashboard') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href="{{ route('dashboard') }}"><i class="fa-solid fa-house"></i> <span class=" ">Dashboard</span></a></li>
+                <li class="navlink"><a class=" {{ request()->routeIs('transactions.*') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href="{{ route('transactions.index') }}"><i class="fa-solid fa-receipt"></i> <span class=" ">Transactions</span></a></li>
+                <li class="navlink"><a class=" {{ request()->routeIs('profile') ? "bg-blue-500 text-white" : "text-gray-300 hover:bg-gray-500/50 hover:text-white" }} text-sm font-semibold flex items-center gap-2 mb-1 py-2.5 px-2.5 rounded-sm" href=""><i class="fa-solid fa-user"></i> <span class=" ">Profile</span></a></li>
+                <li class=" sidebar-toggle mt-auto"><a class=" text-sm font-bold flex items-center gap-2 mb-1 py-2.5 px-2 rounded-sm bg-white text-blue-600" href="#"><i class="fa-solid fa-angles-right fa-xl rotate-180"></i> <span class=" text-[17px]">Collapse</span></a></li>
+            </ul>
+        </aside>
+        <div class=" right-page h-screen flex flex-col ml-16 md:ml-60">
+            <header class=" py-4.5 bg-white shadow-xs z-10 ">
+                <h2 class=" text-secondary text-2xl font-bold text-center ">Budget Tracker</h2>
+            </header>
+            <main class=" flex flex-1 justify-center bg-backgr ">
+            </main>
         </div>
     </body>
 </html>
