c1830cb5d4f8e4c36d1833561fd192cb907e2cb1
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-21 23:54:40 +0100

    add transactions and dashboard routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\TransactionController;
 
 Route::get("/login", [AuthenticationController::class, 'index']);
 Route::get("/register", [AuthenticationController::class, 'register']);
 Route::post("/register", [AuthenticationController::class, 'store']);
+
+Route::view("/", "dashboard")->name('dashboard');
+
+Route::prefix('transactions')->name('transactions.')->group(function () {
+    Route::get("/", [TransactionController::class, 'index'])->name('index');
+    Route::get("/create", [TransactionController::class, 'create'])->name('create');
+    Route::post("/store", [TransactionController::class, 'store'])->name('store');
+});
