69f5f06b1d6335f72c6da8f052b1056fcfa05aec
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-22 11:27:53 +0100

    add fillable array to allow mass assignment of data in columns

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,15 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
+ * @property int $id
+ * @property string $title
+ * @property numeric $amount
  * @property TransactionType $type
- * @property-read \App\Models\User|null $user
+ * @property string|null $notes
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon $date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereNotes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereUserId($value)
  * @mixin \Eloquent
  */
 class Transaction extends Model
 {
+    protected $fillable = ['title', 'amount', 'type', 'date', 'notes', 'user_id'];
     /**
      * Get the attributes that should be cast.
      *
