ca5fac7f7121afbad5907cb8c0d91b17f8580691
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-22 11:26:34 +0100

    handle server error returned for the type field

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -24,14 +24,17 @@
             <p for="amount" class=" text-sm font-medium text-gray-600 mb-2.5 ">TYPE</p>
             <div class=" flex gap-2">
                 <div class=" flex-1 border border-gray-300 rounded-xl py-3 px-3 bg-red-50 flex justify-center items-center gap-2">
-                    <input type="radio" name="type" value="expense" id="expense-radio" checked>
+                    <input type="radio" name="type" class="@error('type') is-invalid @enderror " value="expense" id="expense-radio">
                     <label for="expense-radio" class=" text-lg font-semibold text-red-500">Expense</label>
                 </div>
                 <div class=" flex-1 border border-gray-300 rounded-xl py-3 px-3 bg-green-50 flex justify-center items-center gap-2">
-                    <input type="radio" name="type" value="income" id="income-radio">
+                    <input type="radio" name="type" class="@error('type') is-invalid @enderror " value="income" id="income-radio">
                     <label for="income-radio" class=" text-lg font-semibold text-emerald-600">Income</label>
                 </div>
             </div>
+            @error('type')
+                <div class="text-red-500 text-sm">{{ $message }}</div>
+            @enderror
         </div>
         <div class=" flex flex-col mb-4">
             <label for="date" class=" text-sm font-medium text-gray-600 mb-2.5 ">DATE</label>
@@ -39,7 +42,7 @@
         </div>
         <div class=" flex flex-col mb-4">
             <label for="notes" class=" text-sm font-medium text-gray-600 mb-2.5 ">NOTES</label>
-            <textarea name="notes" id="notes" rows="3" placeholder="Describe this transaction..." value="{{ old('notes') }}" class="border border-gray-300 bg-backgr rounded-md focus:border-blue-500 focus:outline-none pl-3 pt-1.5"></textarea>
+            <textarea name="notes" id="notes" rows="3" placeholder="Describe this transaction..." class="border border-gray-300 bg-backgr rounded-md focus:border-blue-500 focus:outline-none pl-3 pt-1.5">{{ old('notes') }}</textarea>
         </div>
         <div class="flex gap-4">
             <button type="submit" class=" bg-secondary text-white font-semibold px-5 py-3 rounded-md w-full mt-3 cursor-pointer">Save</button>
