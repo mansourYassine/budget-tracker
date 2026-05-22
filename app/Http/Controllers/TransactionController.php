aabea5a0d9d59ddb87067f95e4ac8eb196604cba
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-22 11:25:17 +0100

    Handle create and store new transaction in the controller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -1,17 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Enums\TransactionType;
+use App\Models\Transaction;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\View\View;
 
 class TransactionController extends Controller
 {
+
+    private $userId = 1;
     /**
      * Display a listing of the transaction.
      */
-    public function index()
+    public function index() : View
     {
-        //
+        return view("transactions.index");
     }
 
     /**
@@ -19,15 +28,28 @@ class TransactionController extends Controller
      */
     public function create()
     {
-        //
+        return view("transactions.create");
     }
 
     /**
      * Store a newly created transaction in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'title' => ['required'],
+            'amount' => ['required', 'gt:0'],
+            'type' => ['required', Rule::enum(TransactionType::class)],
+            'date' => ['date', 'nullable'],
+            'notes' => ['string', 'nullable'],
+        ]);
+
+        $validated['user_id'] = $this->userId;
+        $validated['date'] = $validated['date'] ?? now();
+
+        Transaction::create($validated);
+
+        return redirect('/transactions');
     }
 
     /**
