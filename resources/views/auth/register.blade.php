0c571044c2679498a9b107495afbf1ad59d8638f
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-23 16:30:53 +0100

    build the register page using form components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,28 +1,22 @@
 <x-layouts.guest>
     <h2 class=" font-bold text-2xl ">Create Account</h2>
-    <form action="{{ route('register.post') }}" method="post" class="mt-5">
+    <form action="{{ route('register.post') }}" method="post" class="mt-5" novalidate>
         <div class=" flex flex-col mb-4">
             <x-forms.input-label for="form-name" :value="__('FULL NAME')" />
-            <input type="text" name="name" id="form-name" placeholder="John Doe" value="{{ old('name') }}" class=" @error('name') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3">
-            @error('name')
-                <div class="text-red-500 text-sm">{{ $message }}</div>
-            @enderror
+            <x-forms.text-input type="text" name="name" id="form-name" placeholder="John Doe" :value="old('name')" required />
+            <x-forms.input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         <div class=" flex flex-col mb-4">
             <x-forms.input-label for="form-email" :value="__('EMAIL ADDRESS')" />
-            <input type="email" name="email" id="form-email" placeholder="name@email.com"  value="{{ old('email') }}" class=" @error('email') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
-            @error('email')
-                <div class="text-red-500 text-sm">{{ $message }}</div>
-            @enderror
+            <x-forms.text-input type="email" name="email" id="form-email" placeholder="name@email.com" :value="old('email')" required />
+            <x-forms.input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
         <div class=" flex flex-col mb-4">
             <x-forms.input-label for="form-pass" :value="__('PASSWORD')" />
-            <input type="password" name="password" id="form-pass" placeholder="••••••••" class=" @error('password') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
-            @error('password')
-                <div class="text-red-500 text-sm">{{ $message }}</div>
-            @enderror
+            <x-forms.text-input type="password" name="password" id="form-pass" placeholder="••••••••" :value="old('password')" required />
+            <x-forms.input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-        <button type="submit" class=" bg-secondary text-white font-medium px-5 py-3 rounded-md w-full mt-3 cursor-pointer">Create Account</button>
+        <x-forms.button class="w-full">Create Account</x-forms.button>
     </form>
     @push('scripts')
         @vite('resources/js/pages/register.js')
