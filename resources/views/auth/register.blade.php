62bb45130a7322fe1f934be8316177cc78f348bd
Yassine Ait Mansour <yassineaitman@gmail.com>
2026-05-23 13:34:19 +0100

    create input label component and use it in the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,21 +2,21 @@
     <h2 class=" font-bold text-2xl ">Create Account</h2>
     <form action="{{ route('register.post') }}" method="post" class="mt-5">
         <div class=" flex flex-col mb-4">
-            <label for="form-name" class=" text-sm font-medium text-gray-600 mb-2.5 ">FULL NAME</label>
+            <x-forms.input-label for="form-name" :value="__('FULL NAME')" />
             <input type="text" name="name" id="form-name" placeholder="John Doe" value="{{ old('name') }}" class=" @error('name') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3">
             @error('name')
                 <div class="text-red-500 text-sm">{{ $message }}</div>
             @enderror
         </div>
         <div class=" flex flex-col mb-4">
-            <label for="form-email"  class=" text-sm font-medium text-gray-600 mb-2.5 ">EMAIL ADDRESS</label>
+            <x-forms.input-label for="form-email" :value="__('EMAIL ADDRESS')" />
             <input type="email" name="email" id="form-email" placeholder="name@email.com"  value="{{ old('email') }}" class=" @error('email') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
             @error('email')
                 <div class="text-red-500 text-sm">{{ $message }}</div>
             @enderror
         </div>
         <div class=" flex flex-col mb-4">
-            <label for="form-pass"  class=" text-sm font-medium text-gray-600 mb-2.5 ">PASSWORD</label>
+            <x-forms.input-label for="form-pass" :value="__('PASSWORD')" />
             <input type="password" name="password" id="form-pass" placeholder="••••••••" class=" @error('password') is-invalid @enderror border border-gray-300 bg-backgr rounded-md h-9 focus:border-blue-500 focus:outline-none pl-3 ">
             @error('password')
                 <div class="text-red-500 text-sm">{{ $message }}</div>
